d95a1ff1e7e38a2d8b73508d7f2bbca9bdf21792
bilaliqbal1 <muhammadbilaliqbal98@gmail.com>
2020-12-25 22:55:45 +0500

    done tour project

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, price, name, removeTour }) => {
+  const [more, setMore] = useState(false)
+  return <article className="single-tour">
+    <img src={image} alt={name}/>
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{more? info:`${info.substring(0,100)}...`}
+      <button onClick={()=>setMore(!more)}>
+        {more? 'Show less':'more'}
+      </button>
+      </p>
+      <button className="delete-btn" onClick={()=>removeTour(id)}>Not Interested </button>
+    </footer>
+  </article>
 };
 
 export default Tour;
--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import Review from './Review';
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+//react icons he jis me se koe bhi laga sakhte hain
+// jus us ka link dalna hoga
+// import { FaGithubSquare } from 'react-icons/fa';
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>reviews project setup</h2>;
+  // return <h2>Tours Project Setup
+  //   {/* <FaGithubSquare /> */}
+  // </h2>
+  return <main>
+    <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+    </section>
+  </main>
 }
 
-export default App;
+export default App
