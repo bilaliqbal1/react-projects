1e0f11465c29c6e204a2779a1e971795c9a479fe
bilaliqbal1 <muhammadbilaliqbal98@gmail.com>
2021-01-05 15:58:49 +0500

    completed menu project

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -4,7 +4,28 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items);
+  const [categories, setCategories] = useState([]);
+  const allCategories = ['all',...new Set (items.map((item) => item.category))];
+  // console.log(allCategories)
+  const filter = (category) =>{
+    if (category === 'all') {
+      setMenu(items);
+      return;
+    }
+    const newItem = items.filter((item)=> item.category === category)
+    setMenu(newItem)
+  }
+  return <main>
+  <section className="menu section">
+    <div className="title">
+      <h2>Our menu</h2>
+      <div className="underline"></div>
+    </div>
+    <Categories categories={allCategories} filter={filter}/>
+    <Menu items={menu}/>
+  </section>
+  </main>;
 }
 
 export default App;
